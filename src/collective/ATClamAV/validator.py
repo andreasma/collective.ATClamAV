5494bf5aefba5d8ea387637cc6e033ae822856e8
David Glick <dglick@gmail.com>
2011-09-07 17:27:15 -0700

    checkpoint: z3c.form version of the validator

--- src/collective/ATClamAV/validator.py
+++ src/collective/ATClamAV/validator.py
@@ -5,7 +5,7 @@ from Products.CMFCore.interfaces import ISiteRoot
 from Products.CMFCore.utils import getToolByName
 from Products.validation.interfaces.IValidator import IValidator
 from zope.component import getUtility
-from zope.interface import implements
+from zope.interface import implements, Invalid
 
 from collective.ATClamAV.interfaces import IAVScanner
 from collective.ATClamAV.scanner import ScanError
@@ -13,7 +13,33 @@ from collective.ATClamAV.scanner import ScanError
 logger = logging.getLogger('collective.ATClamAV')
 
 
+def _scanBuffer(buffer):
+
+    if Globals.DevelopmentMode: # pragma: no cover
+        logger.warn('Skipping virus scan in development mode.')
+        return ''
+
+    siteroot = getUtility(ISiteRoot)
+    ptool = getToolByName(siteroot, 'portal_properties')
+    settings = ptool.clamav_properties
+    scanner = getUtility(IAVScanner)
+
+    if settings.clamav_connection == 'net':
+        result = scanner.scanBuffer(
+            buffer, 'net',
+            host=settings.clamav_host,
+            port=int(settings.clamav_port),
+            timeout=float(settings.clamav_timeout))
+    else:
+        result = scanner.scanBuffer(buffer, 'socket',
+            socketpath=settings.clamav_socket,
+            timeout=float(settings.clamav_timeout))
+
+    return result
+
+
 class ClamAVValidator:
+    """Archetypes validator to confirm a file upload is virus-free."""
 
     implements(IValidator)
 
@@ -21,10 +47,6 @@ class ClamAVValidator:
         self.name = name
 
     def __call__(self, value, *args, **kwargs):
-        if Globals.DevelopmentMode: # pragma: no cover
-            logger.warn('Skipping virus scan in development mode.')
-            return True
-
         if hasattr(value, 'seek'):
             # when submitted a new file 'value' is a
             # 'ZPublisher.HTTPRequest.FileUpload'
@@ -33,27 +55,13 @@ class ClamAVValidator:
                 # validation is called multiple times for the same file upload
                 return True
 
-            siteroot = getUtility(ISiteRoot)
-            ptool = getToolByName(siteroot, 'portal_properties')
-            settings = ptool.clamav_properties
-            scanner = getUtility(IAVScanner)
-
             value.seek(0)
             # TODO this reads the entire file into memory, there should be
             # a smarter way to do this
             content = value.read()
             result = ''
             try:
-                if settings.clamav_connection == 'net':
-                    result = scanner.scanBuffer(
-                        content, 'net',
-                        host=settings.clamav_host,
-                        port=int(settings.clamav_port),
-                        timeout=float(settings.clamav_timeout))
-                else:
-                    result = scanner.scanBuffer(content, 'socket',
-                        socketpath=settings.clamav_socket,
-                        timeout=float(settings.clamav_timeout))
+                result = _scanBuffer(content)
             except ScanError as e:
                 logger.error('ScanError %s on %s.' % (e, value.filename))
                 return "There was an error while checking the file for " \
@@ -69,3 +77,42 @@ class ClamAVValidator:
         else:
             # if we kept existing file
             return True
+
+
+try:
+    from z3c.form import validator
+    from plone.namedfile.interfaces import INamedField
+    from plone.formwidget.namedfile.interfaces import INamedFileWidget
+    from plone.formwidget.namedfile.validator import NamedFileWidgetValidator
+except ImportError:
+    pass
+else:
+    
+    class Z3CFormClamAVValidator(NamedFileWidgetValidator):
+        """z3c.form validator to confirm a file upload is virus-free."""
+
+        def validate(self, value):
+            super(Z3CFormClamAVValidator, self).validate(value)
+
+            if getattr(value, '_validate_isVirusFree', False):
+                # validation is called multiple times for the same file upload
+                return
+
+            # TODO this reads the entire file into memory, there should be
+            # a smarter way to do this
+            result = ''
+            try:
+                result = _scanBuffer(value.data)
+            except ScanError as e:
+                logger.error('ScanError %s on %s.' % (e, value.filename))
+                raise Invalid("There was an error while checking the file for " \
+                    "viruses: Please contact your system administrator.")
+
+            if result:
+                raise Invalid("Validation failed, file is virus-infected. (%s)" % \
+                    (result))
+            else:
+                # mark the file instance as already checked
+                value._validate_isVirusFree = True
+
+    validator.WidgetValidatorDiscriminators(Z3CFormClamAVValidator, field=INamedField, widget=INamedFileWidget)
